dac602d1311669ef114db0a989faf857571e99b9
Vítor Cardoso Bertolucci <vitorbertolucci@gmail.com>
2020-11-09 18:50:04 -0300

    feat: set up axios
    adds basic axios setup and react-cached-fetch context.

--- App.tsx
+++ App.tsx
@@ -2,14 +2,25 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { CachedFetchProvider } from "react-cached-fetch";
 
 import Navigator from "./src/routes"
+import api from './src/services/api';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <Navigator />
+      <CachedFetchProvider
+        globalOptions={{
+          fetcher: async (route: string) => {
+            const { data } = await api.get(route);
+            return data;
+          }
+        }}
+      >
+        <StatusBar barStyle="dark-content" />
+        <Navigator />
+      </CachedFetchProvider>
     </NavigationContainer>
   );
 };
