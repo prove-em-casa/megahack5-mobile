b7c9c1c7402607f65f11aeeeaaf3daff879447d2
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-09 19:41:48 -0300

    refactor: change the screen name to ShopList

--- src/routes/TabNavigator.tsx
+++ src/routes/TabNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Home from '../screens/HomeScreen';
+import ShopList from '../screens/ShopList';
 import MyAccount from '../screens/MyAccount';
 
 const Tab = createBottomTabNavigator();
@@ -9,7 +9,7 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="ShopList" component={ShopList} />
       <Tab.Screen name="MyAccount" component={MyAccount} />
     </Tab.Navigator>
   )
--- src/screens/ShopList/index.tsx
+++ src/screens/ShopList/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 
 import { Container } from "./styles";
 
-const Home = () => {
+const ShopList = () => {
   return (
     <Container />
   )
 }
 
-export default Home;
+export default ShopList;
