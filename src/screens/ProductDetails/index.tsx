7b5904822cb4fc34eda728610239f059e821d8bf
thcarvalho <th29.br@gmail.com>
2020-11-10 15:03:45 -0300

    feat: add layout and styles for ProductDetails screen

---
+++ src/screens/ProductDetails/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Container,
+  DefaultButton,
+  DefaultButtonText,
+  Header,
+  HeaderText,
+  BodyText,
+} from '../../styles/global';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {
+  AvailableText,
+  DetailsContainer,
+  PriceText,
+  SizeBlock,
+  SizesContainer,
+} from './styles';
+
+const camisetaImg = require('../../../assets/img/camiseta-1.png');
+
+const ProductDetails = () => {
+  return (
+    <Container>
+      <Header>
+        <TouchableOpacity>
+          <Icon name="chevron-back" size={26} color="#fff" />
+        </TouchableOpacity>
+        <HeaderText>DETALHES</HeaderText>
+        <View />
+      </Header>
+
+      <DetailsContainer>
+        <Image source={camisetaImg} />
+        <AvailableText>Disponivel</AvailableText>
+        <BodyText>Camiseta preta - masculina</BodyText>
+        <PriceText>R$ 39,90</PriceText>
+      </DetailsContainer>
+
+      <SizeBlock>
+        <SizesContainer>
+          <Text>P</Text>
+        </SizesContainer>
+        <SizesContainer>
+          <Text>M</Text>
+        </SizesContainer>
+        <SizesContainer>
+          <Text>G</Text>
+        </SizesContainer>
+        <SizesContainer>
+          <Text>GG</Text>
+        </SizesContainer>
+      </SizeBlock>
+
+      <DefaultButton>
+        <DefaultButtonText>Adicionar a sacola</DefaultButtonText>
+      </DefaultButton>
+    </Container>
+  );
+};
+
+export default ProductDetails;
---
+++ src/screens/ProductDetails/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const DetailsContainer = styled.View`
+  width: 164px;
+  height: 237px;
+  border-color: ${colors.secundary_gray};
+  border-radius: 4px;
+  border-width: 1px;
+  background-color: #fff;
+  padding: 15px 6px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const AvailableText = styled.Text`
+  color: ${colors.main_green};
+  font-size: 10px;
+`;
+
+export const PriceText = styled.Text`
+  font-family: NunitoSans-ExtraBold;
+  font-size: 14px;
+  color: ${colors.text_gray};
+`;
+
+export const SizeBlock = styled.View`
+  margin-top: 32px;
+  flex-direction: row;
+  margin-bottom: 135px;
+`;
+
+export const SizesContainer = styled.View`
+  width: 38px;
+  height: 40px;
+  border-color: ${colors.secundary_gray};
+  border-radius: 4px;
+  border-width: 1px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 6.5px;
+`;
