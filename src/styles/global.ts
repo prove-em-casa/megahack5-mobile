8cbe677a8d3c85871c3d8f57bb4a389d308f8286
Vítor Cardoso Bertolucci <vitorbertolucci@gmail.com>
2020-11-10 09:22:55 -0300

    style: adds default button styles
    Adds DefaultButton and DefaultButtonText styled-components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components/native';
+
 import colors from './colors';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background: ${colors.background_white};
 `;
 
 export const Header = styled.View`
@@ -23,4 +25,29 @@ export const HeaderText = styled.Text`
   color: #fff;
   font-family: NunitoSans-ExtraBold;
   font-size: 14px;
+`;
+
+export const BodyText = styled.Text`
+  font-family: NunitoSans-Regular;
+  font-size: 12px;
+  padding: 8px 0;
+  color: ${colors.text_gray};
+`;
+
+export const DefaultButton = styled.TouchableOpacity`
+  background: ${colors.main_red};
+  border-radius: 18px;
+  width: 280px;
+  height: 40px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DefaultButtonText = styled.Text`
+  font-family: NunitoSans-Regular;
+  font-size: 16px;
+  line-height: 21.82px;
+  color: #ffffff;
 `;
