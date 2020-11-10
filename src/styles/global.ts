6297bb63a4c8e853cd2c46e0a7e4d5e26bcefa7f
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-10 00:56:02 -0300

    feat: add header and header text styled components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,26 @@
 import styled from 'styled-components/native';
+import colors from './colors';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+`;
+
+export const Header = styled.View`
+  position: absolute;
+  top: 0;
+  height: 56px;
+  width: 100%;
+  background-color: ${colors.main_red};
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+export const HeaderText = styled.Text`
+  color: #fff;
+  font-family: NunitoSans-ExtraBold;
+  font-size: 14px;
 `;
