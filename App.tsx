dbcf7528349751c940a09452d0b3e778f32bf5e9
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-10 00:57:04 -0300

    feat: add configs for support custom fonts

--- App.tsx
+++ App.tsx
@@ -3,11 +3,24 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { CachedFetchProvider } from 'react-cached-fetch';
+import * as Font from 'expo-font';
 
 import Navigator from './src/routes';
 import api from './src/services/api';
+import colors from './src/styles/colors';
+import { isLoaded } from 'expo-font';
 
 const App = () => {
+  const [loaded] = Font.useFonts({
+    NunitoRegular: require('./assets/fonts/NunitoSans-Regular.ttf'),
+    NunitoSemiBold: require('./assets/fonts/NunitoSans-SemiBold.ttf'),
+    NunitoExtraBold: require('./assets/fonts/NunitoSans-ExtraBold.ttf'),
+  });
+
+  if (!isLoaded('NunitoRegular')) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <CachedFetchProvider
@@ -17,7 +30,7 @@ const App = () => {
             return data;
           },
         }}>
-        <StatusBar barStyle="dark-content" />
+        <StatusBar backgroundColor={colors.main_red} />
         <Navigator />
       </CachedFetchProvider>
     </NavigationContainer>
