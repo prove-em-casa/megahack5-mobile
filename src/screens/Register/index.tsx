f760eb60dccc77a56a2aa8a43533f3d88578a95f
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-12 14:28:04 -0300

    feat: implements connection with backend api for register user

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { ToastAndroid, TouchableOpacity, View } from 'react-native';
 import {
   Container,
   DefaultButton,
@@ -9,22 +9,80 @@ import {
   StyledInput,
 } from '../../styles/global';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+import api from '../../services/api';
 
 const Register = () => {
+  const [name, setName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [confirmPass, setConfirmPass] = useState<string>();
+
+  const { goBack, navigate } = useNavigation();
+
+  async function registerUser() {
+    if (!name || !email || !password) {
+      return ToastAndroid.show(
+        'Por favor, preencha todos os campos',
+        ToastAndroid.SHORT,
+      );
+    }
+    if (confirmPass !== password) {
+      return ToastAndroid.show(
+        'As senhas precisam ser iguais',
+        ToastAndroid.SHORT,
+      );
+    }
+
+    api
+      .post('/users/create', {
+        name,
+        email,
+        password,
+      })
+      .then(() => {
+        ToastAndroid.show('Cadastro efetuado com sucesso', ToastAndroid.SHORT);
+        navigate('Login');
+      })
+      .catch(() => {
+        ToastAndroid.show('E-mail já cadastrado', ToastAndroid.SHORT);
+      });
+  }
+
   return (
     <Container>
       <Header>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={goBack}>
           <Icon name="chevron-back" size={26} color="#fff" />
         </TouchableOpacity>
         <HeaderText style={{ marginRight: 20 }}>CADASTRE-SE</HeaderText>
         <View />
       </Header>
-      <StyledInput placeholder="Seu nome" />
-      <StyledInput placeholder="E-mail" />
-      <StyledInput placeholder="Senha" />
-      <StyledInput placeholder="Confirme a senha" />
-      <DefaultButton style={{ position: 'absolute', bottom: 50 }}>
+      <StyledInput
+        placeholder="Seu nome"
+        value={name}
+        onChangeText={(name) => setName(name)}
+      />
+      <StyledInput
+        placeholder="E-mail"
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+      />
+      <StyledInput
+        placeholder="Senha"
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+        secureTextEntry={true}
+      />
+      <StyledInput
+        placeholder="Confirme a senha"
+        value={confirmPass}
+        onChangeText={(confirmPass) => setConfirmPass(confirmPass)}
+        secureTextEntry={true}
+      />
+      <DefaultButton
+        onPress={registerUser}
+        style={{ position: 'absolute', bottom: 50 }}>
         <DefaultButtonText>Confirmar</DefaultButtonText>
       </DefaultButton>
     </Container>
