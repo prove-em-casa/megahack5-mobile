20b8114f744c9afc8b58530f8a86ad700c67ac9a
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-12 14:29:54 -0300

    feat: implements connection with backend for user authentication

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { ToastAndroid, TouchableOpacity } from 'react-native';
+import api from '../../services/api';
 import {
   Container,
   DefaultButton,
@@ -15,17 +18,52 @@ import {
 } from './styles';
 
 const Login = () => {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const { navigate } = useNavigation();
+
+  async function authenticateUser() {
+    if (!email || !password) {
+      return ToastAndroid.show(
+        'Por favor, preencha todos os campos',
+        ToastAndroid.SHORT,
+      );
+    }
+    api
+      .post('/tokens', {
+        email,
+        password,
+      })
+      .then((response) => {
+        AsyncStorage.setItem('@sessionToken', response.data.token);
+        navigate('Home');
+      })
+      .catch(() => {
+        ToastAndroid.show('E-mail ou senha inválidos', ToastAndroid.SHORT);
+      });
+  }
+
   return (
     <Container>
       <AlignBlock>
         <AccountText>Ainda não tem uma conta?</AccountText>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigate('Register')}>
           <CreateAccountText>Criar Conta</CreateAccountText>
         </TouchableOpacity>
       </AlignBlock>
-      <StyledInput placeholder="E-mail" />
-      <StyledInput placeholder="Senha" style={{ marginBottom: 48 }} />
-      <DefaultButton>
+      <StyledInput
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+        placeholder="E-mail"
+      />
+      <StyledInput
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+        placeholder="Senha"
+        style={{ marginBottom: 48 }}
+        secureTextEntry={true}
+      />
+      <DefaultButton onPress={authenticateUser}>
         <DefaultButtonText>ENTRAR</DefaultButtonText>
       </DefaultButton>
       <TouchableOpacity>
