f78134e1d55d5351422e2691ab33f5a39b450e91
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-12 21:59:53 -0300

    feat: add header and back button

--- src/screens/CreditCardList/styles.ts
+++ src/screens/CreditCardList/styles.ts
@@ -2,9 +2,7 @@ import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 export const CreditCardsListContainer = styled.View`
-  padding: 10px;
   width: 100%;
-
   align-items: center;
   justify-content: center;
 `;
